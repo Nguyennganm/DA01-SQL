9eec35552808e6dc952095d132140df3241a7d87
Nguyennganm <166844701+Nguyennganm@users.noreply.github.com>
2024-04-30 22:44:17 +0700

    Update PRACTICE_06.sql

--- PRACTICE_06.sql
+++ PRACTICE_06.sql
@@ -56,5 +56,66 @@ GROUP BY user_id,max_date)
 select month, COUNT(user_id) as monthly_active_users from active_users
 GROUP BY month
 --EX06:
-
+select DATE_FORMAT(trans_date, "%Y-%m") AS month, 
+country,
+COUNT(id) AS trans_count, 
+SUM(IF(state = 'approved', 1, 0)) AS approved_count, 
+SUM(amount) AS trans_total_amount, 
+SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country;
+--EX07:
+SELECT product_id, 
+year as first_year, 
+quantity,
+price
+FROM Sales
+WHERE (product_id,year) in 
+(SELECT product_id,MIN(year)
+FROM Sales
+GROUP BY product_id);
+--EX08:
+select customer_id from customer 
+group by customer_id
+having count(distinct product_key ) = (select count(product_key) from product);
+--EX09:
+SELECT employee_id
+FROM Employees as a
+WHERE manager_id not in (SELECT employee_id FROM employees) and salary < 30000
+ORDER BY employee_id;
+--EX10:
+with duplicate_jobs as 
+(select company_id, title, description, count(job_id) as count_job
+from job_listings
+group by company_id, title, description
+order by company_id)
+select count(company_id) as duplicate_companies
+from duplicate_jobs
+where count_job > 1;
+--EX11:
+(select name as results 
+  from movierating mr inner join users u 
+  on mr.user_id=u.user_id
+  group by u.user_id 
+  order by count(*) desc, name asc 
+  limit 1)
+union all
+(select results from
+  (select title as results, avg(rating) as average_rating 
+  from movierating mr 
+  inner join movies m 
+  on mr.movie_id=m.movie_id 
+  where month(created_at) = 2 
+  group by m.movie_id) rating_group
+  order by average_rating desc, results asc limit 1);
+--EX12:
+select id, count(id) num from
+(select requester_id as id
+from RequestAccepted
+union all
+select accepter_id as id
+from RequestAccepted) a
+group by id
+order by count(id) desc
+limit 1;
 
